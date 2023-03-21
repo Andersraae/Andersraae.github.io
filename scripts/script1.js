c52e31dd1674e4cdbb39d1fd7003af6c9661800a
Anders <andersraae01@gmail.com>
2023-03-21 09:13:31 +0100

    Småting til statisk hjemmsideø

---
+++ scripts/script1.js
@@ -0,0 +1,9 @@
+console.log('der er noget tekst');
+document.getElementById('mybtn').addEventListener("click", function(){ 
+    alert('hdsfknskdjf'); 
+    console.log('oinasdfpoj');
+});
+document.getElementById('mybtn').addEventListener('click', (event) => {
+    alert('hdsfknskdjf'); 
+    console.log('oinasdfpoj');
+});
